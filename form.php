813803835701d9819d52468421e55976ceca407f
Maria <mgordillo@bionexo.com>
2021-11-11 23:52:02 -0300

    Comitando arquivos do projeto php

---
+++ form.php
@@ -0,0 +1,37 @@
+<?php
+
+// Variável para coleta dos dados de erro
+//$erro = false;
+
+//Tamanho mínimo de caracteres para nome
+
+$nome = (isset($_POST['nome']));
+if(strlen($nome)<2){
+echo ("Preencha o nome com no mínimo 2 caracteres.". "<br> <br>");
+}
+else{
+echo ("O nome cadastrado foi: <br>" . $_POST['nome'] . '!' . "<br> <br>" );
+}
+
+//Validação do email com filtro php:
+
+
+function validateEmail($email){
+//$email = (isset($_POST['email']));
+if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+echo ("E-mail válido. O email cadastrado foi: <br>" . $_POST['email'] . "<br> <br>");    
+}else{
+echo("E-mail inválido" );
+}
+}
+
+// Listamos os erros existentes
+//if ( $erro ) {
+    //echo $erro;
+//}   
+
+//if(isset($_POST['submit'])) {
+    //echo("O nome cadastrado foi: <br>" . $_POST['nome'] . '!' . "<br> <br>" );
+    //echo("O email cadastrado foi: <br>" . $_POST['email'] . "<br> <br>" );
+//}
+
---
+++ script.js
@@ -0,0 +1,43 @@
+const formulário = document.getElementById('formulário') 
+
+
+formulário.addEventListener('submit', (e) => { 
+
+e.preventDefault(); //impedir o evento de recarregar
+
+let nome = document.getElementById('nome').value; 
+
+let email = document.getElementById('email').value; 
+
+let data = {
+    nome,
+    email,
+}
+let convertData = JSON.stringify(data);
+
+localStorage.setItem("lead", convertData)
+
+})
+
+//Validação do campo e-mail
+
+function emailValidation(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+    
+    if ((usuario.length >=1) &&
+        (dominio.length >=3) &&
+        (usuario.search("@")==-1) &&
+        (dominio.search("@")==-1) &&
+        (usuario.search(" ")==-1) &&
+        (dominio.search(" ")==-1) &&
+        (dominio.search(".")!=-1) &&
+        (dominio.indexOf(".") >=1)&&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    //document.getElementById("msgemail").innerHTML="E-mail válido";
+    alert("Informações válidas! Clique para se cadastrar em nossa plataforma!");
+    }
+    else{
+    alert("E-mail inválido, por favor, verifique os dados cadastrados.");
+    }
+    }
