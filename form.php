b5474b5cda772707d3b64672e032246364050a88
Maria <mgordillo@bionexo.com>
2021-11-12 23:27:17 -0300

    Atualização

--- form.php
+++ form.php
@@ -1,26 +1,29 @@
 <?php
 
-$nome = (isset($_POST['nome']));
+//$nome = (isset($_POST["nome"]));
 
-$email = (isset($_POST['email']));
+//$email = (isset($_POST["email"]));
 
 echo "<h2>Seus dados cadastrados</h2>";
 echo "<br>";
 echo "<br>";
 
-if(strlen($nome)<=2){
-    echo ("Preencha o nome com no mínimo 2 caracteres.". "<br> <br>");
+if((strlen($_POST["nome"]))<=2){
+
+    //validar se entrou sobrenome também
+    echo (" Preencha o nome com no mínimo 2 caracteres." . "<br> <br>");
 }else{
-    echo ("O nome cadastrado foi: <br>" . $nome . "<br> <br>" );
+    echo ("O nome cadastrado foi: <br>" . $_POST["nome"] . "<br> <br>" );
 }
 
 echo "<br>";
 
-if(filter_var ($email, FILTER_VALIDATE_EMAIL)){
-    echo ("E-mail válido. O email cadastrado foi: <br>" . $email . "<br> <br>");
+if(filter_var ($_POST["email"], FILTER_VALIDATE_EMAIL)){
+    echo("E-mail válido. O email cadastrado foi: <br>" . $_POST["email"] . "<br> <br>");
 }else{
     echo("E-mail inválido.");
 }
 
 echo "<br>";
+
 
